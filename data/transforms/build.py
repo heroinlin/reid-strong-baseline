e77ad13e63e95969532a952a30c4552c00b662ee
heroinlj <heroinlj@gmail.com>
2019-03-27 14:20:57 +0800

    修改transform，适应opencv读入

--- data/transforms/build.py
+++ data/transforms/build.py
@@ -6,18 +6,25 @@
 
 import torchvision.transforms as T
 
-from .transforms import RandomErasing
+from .transforms import RandomErasing, Random2DTranslation, RandomFlip, ColorAugmentation
+from .opencv_transforms import ColorJitter as ColorJitter_cv
+from .opencv_transforms import Resize as Resize_cv
+from .opencv_transforms import RandomCrop as RandomCrop_cv
+from .opencv_transforms import Pad as Pad_cv
 
 
 def build_transforms(cfg, is_train=True):
     normalize_transform = T.Normalize(mean=cfg.INPUT.PIXEL_MEAN, std=cfg.INPUT.PIXEL_STD)
     if is_train:
         transform = T.Compose([
-            T.Resize(cfg.INPUT.SIZE_TRAIN),
-            T.RandomHorizontalFlip(p=cfg.INPUT.PROB),
-            T.Pad(cfg.INPUT.PADDING),
-            T.RandomCrop(cfg.INPUT.SIZE_TRAIN),
+            Resize_cv(tuple(cfg.INPUT.SIZE_TRAIN)),
+            RandomCrop_cv(tuple(cfg.INPUT.SIZE_TRAIN)),
+            Pad_cv(cfg.INPUT.PADDING),
+            ColorJitter_cv(brightness=0.2, contrast=0.15, saturation=0, hue=0),
             T.ToTensor(),
+            RandomFlip(probability=cfg.INPUT.PROB, dim=1),
+            RandomFlip(probability=cfg.INPUT.PROB, dim=2),
+            ColorAugmentation(),
             normalize_transform,
             RandomErasing(probability=cfg.INPUT.RE_PROB, mean=cfg.INPUT.PIXEL_MEAN)
         ])
--- data/transforms/transforms.py
+++ data/transforms/transforms.py
@@ -6,6 +6,34 @@
 
 import math
 import random
+import torch
+import cv2
+
+
+class Random2DTranslation(object):
+    """
+    With a probability, first increase image size to (1 + 1/8), and then perform random crop.
+
+    Args:
+    - height (int): target image height.
+    - width (int): target image width.
+    - p (float): probability of performing this transformation. Default: 0.5.
+    """
+
+    def __init__(self, height, width, p=0.5):
+        self.height = height
+        self.width = width
+        self.p = p
+
+    def __call__(self, img):
+        new_width, new_height = int(round(self.width * 1.125)), int(round(self.height * 1.125))
+        resized_img = cv2.resize(img, dsize=(new_width, new_height))
+        x_maxrange = new_width - self.width
+        y_maxrange = new_height - self.height
+        x1 = int(round(random.uniform(0, x_maxrange)))
+        y1 = int(round(random.uniform(0, y_maxrange)))
+        croped_img = resized_img[x1: x1 + self.width, y1: y1 + self.height, :]
+        return croped_img
 
 
 class RandomErasing(object):
@@ -53,3 +81,61 @@ class RandomErasing(object):
                 return img
 
         return img
+
+
+class RandomFlip(object):
+    """
+        Randomly Flip the Tensor
+
+        Reference:
+        probability: The probability that the operation will be performed.
+        dim: The dim that will be flip, 1 is VerticalFlip, 2 is HorizontalFlip
+        https://github.com/pytorch/pytorch/issues/229
+        """
+
+    def __init__(self, probability=0.5, dim=1):
+        self.probability = probability
+        self.dim = dim
+
+    def _check_input(self, tensor):
+        assert tensor.dim() == 3 and tensor.size(0) == 3
+
+    def flip(self, x, dim):
+        indices = [slice(None)] * x.dim()
+        indices[dim] = torch.arange(x.size(dim) - 1, -1, -1,
+                                    dtype=torch.long, device=x.device)
+        return x[tuple(indices)]
+
+    def __call__(self, tensor):
+        if random.uniform(0, 1) > self.probability:
+            return tensor
+        return self.flip(tensor, self.dim)
+
+
+class ColorAugmentation(object):
+    """
+    Randomly alter the intensities of RGB channels
+
+    Reference:
+    Krizhevsky et al. ImageNet Classification with Deep ConvolutionalNeural Networks. NIPS 2012.
+    """
+
+    def __init__(self, p=0.5):
+        self.p = p
+        self.eig_vec = torch.Tensor([
+            [0.4009, 0.7192, -0.5675],
+            [-0.8140, -0.0045, -0.5808],
+            [0.4203, -0.6948, -0.5836],
+        ])
+        self.eig_val = torch.Tensor([[0.2175, 0.0188, 0.0045]])
+
+    def _check_input(self, tensor):
+        assert tensor.dim() == 3 and tensor.size(0) == 3
+
+    def __call__(self, tensor):
+        if random.uniform(0, 1) > self.p:
+            return tensor
+        alpha = torch.normal(mean=torch.zeros_like(self.eig_val)) * 0.1
+        quatity = torch.mm(self.eig_val * alpha, self.eig_vec)
+        tensor = tensor + quatity.view(3, 1, 1)
+        return tensor
